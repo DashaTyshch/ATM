e66c01bbb4eba33db7b8c35e0dbdfadee1ddf9ff
Viktoria Kozopas <kozopasv@gmail.com>
2018-11-17 13:46:42 +0200

    Login page add Credentials model
    add package for work with Http requests
    Login button click

---
+++ ATM/DataModels/Credentials.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.DataModels
+{
+    class Credentials
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
+
+/*
+ * using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TKTBanking.Models
+{
+    public class Credentials
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
+ */
--- ATM/Pages/LoginPage.xaml.cs
+++ ATM/Pages/LoginPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,65 @@ namespace ATM
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void GetData()
+        {
+            HttpClient client = new HttpClient();
+            //client.BaseAddress = new Uri("http://localhost:56851/");
+            client.BaseAddress = new Uri("https://tktbanking.azurewebsites.net/");
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("api/auth/signin").Result; // auth/signin ?????/
+
+            if (response.IsSuccessStatusCode)
+            {
+               var user = response.Content.ReadAsAsync<IEnumerable<DataModels.Credentials>>().Result;
+                // TODO:: and where futher?
+
+               // usergrid.ItemsSource = users;
+
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+
+            //client.BaseAddress = new Uri("http://localhost:56851/");
+
+            client.BaseAddress = new Uri("https://tktbanking.azurewebsites.net/");
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var userCredentials = new DataModels.Credentials();
+
+            userCredentials.Login = LoginTextBox.Text;
+            userCredentials.Password = PasswordTextBox.Password;
+
+            var response = client.PostAsJsonAsync("api/auth/signin", userCredentials).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("User Logged in");
+                LoginTextBox.Text = "";
+                PasswordTextBox.Password = ""; // ??????????
+                GetData();
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
         }
     }
 }
